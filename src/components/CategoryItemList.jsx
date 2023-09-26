76e0d969c60bf3d5d3005caec17a65b71614697e
JulianBati92 <julian.batistutti@gmail.com>
2023-09-25 22:35:38 -0300

    Gif carga y mensaje de carga

--- src/components/CategoryItemList.jsx
+++ src/components/CategoryItemList.jsx
@@ -3,35 +3,51 @@ import { useParams } from 'react-router-dom';
 
 const CategoryItemList = () => {
   const [categoryProducts, setCategoryProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const { id } = useParams();
 
   useEffect(() => {
-    // Fetch products based on the category
+    setLoading(true);
+
     fetch(`https://fakestoreapi.com/products/category/${id}`)
       .then((response) => response.json())
-      .then((data) => setCategoryProducts(data));
+      .then((data) => {
+        setCategoryProducts(data);
+        setLoading(false);
+      });
   }, [id]);
 
   return (
     <div className="container mt-4 text-center" style={{ marginBottom: '200px' }}>
       <h1>{id}</h1>
 
-      <div className="row">
-        {categoryProducts.map((product) => (
-          <div className="col-md-4" key={product.id}>
-            <div className="card mb-4">
-              <img src={product.image} className="card-img-top" alt={product.title} />
-              <div className="card-body">
-                <h5 className="card-title">{product.title}</h5>
-                <p className="card-text">{product.description}</p>
-                <p className="card-text">Precio: {product.price} U$S</p>
+      {loading && (
+        <div>
+          <p>Cargando productos...</p>
+          <img src="/loading-45.gif" alt="Cargando productos" style={{ width: '50px', height: '50px' }} />
+        </div>
+      )}
+
+      {!loading && (
+        <div className="row">
+          {categoryProducts.map((product) => (
+            <div className="col-md-4" key={product.id}>
+              <div className="card mb-4">
+                <img src={product.image} className="card-img-top" alt={product.title} />
+                <div className="card-body">
+                  <h5 className="card-title">{product.title}</h5>
+                  <p className="card-text">{product.description}</p>
+                  <p className="card-text">Precio: {product.price} U$S</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
 
 export default CategoryItemList;
+
