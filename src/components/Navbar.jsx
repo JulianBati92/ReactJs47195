12e89529d7cb1269885437b56c3f8578d52e66ed
JulianBati92 <julian.batistutti@gmail.com>
2023-09-25 21:51:40 -0300

    Cart-icon y favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { BiCart } from 'react-icons/bi';
+import CartWidget from './CartWidget'; 
 
 const Navbar = () => {
   const [categories, setCategories] = useState([]);
+  const cartItemCount = 5; // Reemplaza esto con tu lógica real para obtener la cantidad de ítems en el carrito
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products/categories')
@@ -12,40 +15,50 @@ const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="#">
-        Tu Matteoli
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/">
-              Principal
-            </Link>
-          </li>
-          {categories.map(category => (
-            <li className="nav-item" key={category}>
-              <Link className="nav-link" to={`/category/${category}`}>
-                {category}
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          Tu Matteoli
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                Principal
               </Link>
             </li>
-          ))}
-        </ul>
+            {categories.map(category => (
+              <li className="nav-item" key={category}>
+                <Link className="nav-link" to={`/category/${category}`}>
+                  {category}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="ml-auto">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="#">
+                <BiCart className="cart-icon" /> {cartItemCount}
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <CartWidget />
       </div>
     </nav>
   );
 };
 
 export default Navbar;
-
-
