4d370eea7377db373be100965f54fad66da5dd25
JulianBati92 <julian.batistutti@gmail.com>
2023-09-27 17:40:09 -0300

    Se saca descripcion
    se agrega dentro de detalles

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -24,6 +24,7 @@ const ItemListContainer = ({ greeting }) => {
               <img src={product.image} className="card-img-top" alt={product.title} />
               <div className="card-body">
                 <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">Precio: {product.price} U$S </p>
                 <Link to={`/item/${product.id}`} className="btn btn-primary">
                   Detalles
                 </Link>
