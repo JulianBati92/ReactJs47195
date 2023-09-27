0ee8cf48ac68a0e2fc6bc4eecb1f04abd54f18c2
JulianBati92 <julian.batistutti@gmail.com>
2023-09-27 16:17:47 -0300

    Detalles
    Se saca descripción y precio de las card, se agregan dentro de los detalles

--- src/components/CategoryItemList.jsx
+++ src/components/CategoryItemList.jsx
@@ -41,8 +41,6 @@ const CategoryItemList = () => {
                 <img src={product.image} className="card-img-top" alt={product.title} />
                 <div className="card-body">
                   <h5 className="card-title">{product.title}</h5>
-                  <p className="card-text">{product.description}</p>
-                  <p className="card-text">Precio: {product.price} U$S</p>
                   <Link to={`/item/${product.id}`} className="btn btn-primary">
                     Detalles
                   </Link>
